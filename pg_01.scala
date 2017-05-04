d971a24bc8e2b47f01d42d51c1497c2249cd975e
Madusudanan.B.N <b.n.madusudanan@gmail.com>
2017-05-05 00:34:31 +0530

    Update pg_01.scala

--- pg_01.scala
+++ pg_01.scala
@@ -11,10 +11,18 @@ object Runner extends App{
   //Reading csv file
   val df = sparkSession.read.csv("test.csv")
 
+  //Reading ignoring the header
+  val df_no_header = sparkSession.read.option("header","true").csv("test.csv")
+
   //Print content
   df.show()
+
+  df_no_header.show()
 
   //Print the number of rows
   println(df.count())
 
+  println(df_no_header.count())
+
 }
+
